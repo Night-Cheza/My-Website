6743d4d4e591bdcab8da0fd499f6ecd7ea1cd2a7
Leila <leila.kassi@outlook.com>
2024-05-15 19:04:15 -0600

    added header component with initial styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+	return (
+		<>
+				<Header className="header"/>
+		</>
+	);
 }
 
 export default App;
