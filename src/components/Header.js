96c94970b1164b186082759249d22baada1a8b4a
Leila <leila.kassi@outlook.com>
2024-06-11 12:22:11 -0600

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="header">
 			<h1>Welcome to my website</h1>
-			<text>"A jack of all trades is a master of none, but oftentimes better than a master of one." <span>&copy;</span>William Shakespeare</text>
+			<text>"A jack of all trades is a master of none, but oftentimes better than a master of one." <br/> <span>&copy;</span>William Shakespeare</text>
     </header>
   );
 }
