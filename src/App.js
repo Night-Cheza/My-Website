489d5e3e4845f882d3e617cf9d8e9390f749c356
Leila <leila.kassi@outlook.com>
2024-06-12 12:57:59 -0600

    adding functionality to buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
+import Section from './components/Section';
+import {section_info} from './components/section_info';
 
-function App() {
+function App () {
+	const [ selectedSection, setselectedSection ] = useState('home');
+
+	function clickHandler (selectedButton) {
+		setselectedSection(selectedButton);
+	}
+
 	return (
 		<>
-			<Header className="header" />
+			<Header />
 			<div id='links'>
-				<Button>About me</Button>
-				<Button>My vision</Button>
-				<Button>My learns</Button>
-				<Button>Home</Button>
+				<Button onClick={() => clickHandler('about')}>About Me</Button>
+				<Button onClick={() => clickHandler('vision')}>My Vision</Button>
+				<Button onClick={() => clickHandler('learns')}>My Learns</Button>
+				<Button onClick={() => clickHandler('home')}>Home</Button>
 			</div>
-
+			<section>{section_info.map( ( data, _key ) => (
+				selectedSection === data.id ?
+					<Section key={data.id} title={data.title} description={data.description} />
+					: undefined
+			) )}
+			</section>
 		</>
 	);
 }
