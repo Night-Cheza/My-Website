7a75d0c8b63b0bd1284436fbf2d702105b536e07
Leila <leila.kassi@outlook.com>
2024-06-12 12:57:12 -0600

    updating section and header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import './Header.css'
 
 export default function Header() {
   return (
     <header className="header">
 			<h1>Welcome to my website</h1>
-			<text>"A jack of all trades is a master of none, but oftentimes better than a master of one." <br/> <span>&copy;</span>William Shakespeare</text>
+			<p>Leveraging versatility and diverse experiences to drive success</p>
     </header>
   );
 }
--- src/components/section_info.js
+++ src/components/section_info.js
@@ -1,21 +1,21 @@
 export const section_info = [
 	{
-		id: 1,
-		title: "About Me",
-		description: "here is about me"
+		id: 'about',
+		title: 'About Me',
+		description: "I am Miss 'Positivity and Curiosity,' a passionate individual who thrives on exploring new topics, possibilities, and opportunities. My journey has been diverse, encompassing various fields and experiences that have shaped me into a versatile professional.\n\nWith a background in biology and experience in the nursing field, I have developed a solid foundation in scientific thinking and compassion for others. Upon transitioning into the corporate world, I honed my organizational, interpersonal, and administrative skills, which led to a promotion to the role of assistant to the CEO. Most recently, I graduated with a diploma in Information Technology and embarked on a career as a Software Developer.\n\nThroughout my career, I have been recognized for my efficiency, persistence, determination, and analytical thinking. I am a team player who can also work independently, always striving to deliver high-quality results. My client-oriented approach ensures that I meet and exceed expectations, while my fast-learning ability enables me to adapt quickly to new challenges.\n\nI am committed to continuous growth and improvement, both as an individual and as a team player. My goal is to enhance business process standards and contribute to the success of my team and organization. Join me on this journey of positivity and curiosity, where together, we can achieve great things."
 	},
 	{
-		id: 2,
+		id: 'vision',
 		title: "My Vision",
-		description: "here is my vision"
+		description: "'A jack of all trades is a master of none, but oftentimes better than a master of one.' \u00a9William Shakespeare. This saying resonates deeply with my journey through winding personal and professional paths. I believe that it is this diversity and versatility that form the cornerstone of success.\n\nBeing exposed to different professional fields, I bring a unique perspective to every challenge. My vision is to harness the power of varied skills and experiences to drive innovation and excellence. I am committed to fostering an environment where adaptability, continuous learning, and diverse expertise are celebrated as essential elements of growth and achievement.\n\nBy embracing and integrating these diverse pathways, I aim to contribute to a dynamic and inclusive world where versatility is recognized as a strength and a vital component of sustainable success."
 	},
 	{
-		id: 3,
+		id: 'learns',
 		title: "My Learns",
 		description: "here is about whet I'm learning"
 	},
 	{
-		id: 4,
+		id: 'home',
 		title: "Home",
 		description: "home section"
 	}
