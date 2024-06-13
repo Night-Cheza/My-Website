95844137bfe44d6ae50bb9c4717f7d85ebbf060b
Leila <leila.kassi@outlook.com>
2024-06-13 15:38:45 -0600

    updates section component and info

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,22 @@
-export default function Section ({title, description}) {
+import {useEffect, useState} from "react";
+
+export default function Section ( {title, description, image, ...props} ) {
+	const [ display, setDisplay ] = useState( false );
+
+	useEffect( () => {
+		if( title === 'Home' )
+		{
+			setDisplay( true );
+		}
+	}, [title] )
+
 	return (
 		<>
 			<h3>{title}</h3>
-			<p>{description}</p>
+			<div {...props}>
+				<p>{description}</p>
+				{display ? <img alt='' src={image}/> : <div></div>}
+			</div>
 		</>
 	);
 }
--- src/components/section_info.js
+++ src/components/section_info.js
@@ -1,22 +1,24 @@
+import art from '../assets/art1.png';
 export const section_info = [
 	{
-		id: 'about',
-		title: 'About Me',
-		description: "I am Miss 'Positivity and Curiosity,' a passionate individual who thrives on exploring new topics, possibilities, and opportunities. My journey has been diverse, encompassing various fields and experiences that have shaped me into a versatile professional.\n\nWith a background in biology and experience in the nursing field, I have developed a solid foundation in scientific thinking and compassion for others. Upon transitioning into the corporate world, I honed my organizational, interpersonal, and administrative skills, which led to a promotion to the role of assistant to the CEO. Most recently, I graduated with a diploma in Information Technology and embarked on a career as a Software Developer.\n\nThroughout my career, I have been recognized for my efficiency, persistence, determination, and analytical thinking. I am a team player who can also work independently, always striving to deliver high-quality results. My client-oriented approach ensures that I meet and exceed expectations, while my fast-learning ability enables me to adapt quickly to new challenges.\n\nI am committed to continuous growth and improvement, both as an individual and as a team player. My goal is to enhance business process standards and contribute to the success of my team and organization. Join me on this journey of positivity and curiosity, where together, we can achieve great things."
+		'id': 'about',
+		'title': 'About Me',
+		'description': "Miss 'Positivity and Curiosity,' a passionate individual who thrives on exploring new topics, possibilities, and opportunities. My journey has been diverse, encompassing various fields and experiences that have shaped me into a versatile professional.\n\nWith a background in biology and experience in the nursing field, I have developed a solid foundation in scientific thinking and compassion for others. Transitioning into the corporate world gave me the opportunity to hone my organizational, interpersonal, and administrative skills, which led to a promotion to the role of assistant to the CEO. Most recently, I graduated with a diploma in Information Technology and embarked on a career as a Software Developer.\n\nThroughout my career, I have been recognized for my efficiency, persistence, determination, and analytical thinking. As a team player who can also work independently, I always strive to deliver high-quality results. My client-oriented approach ensures that expectations are not just meet but also exceeded, while fast-learning ability enables me to adapt quickly to new challenges.\n\nI am committed to continuous growth and improvement, both as an individual and as a team player. My goal is to enhance business process standards and contribute to the success of my team and organization. Join me on this journey of positivity and curiosity, where together, we can achieve great things.",
 	},
 	{
-		id: 'vision',
-		title: "My Vision",
-		description: "'A jack of all trades is a master of none, but oftentimes better than a master of one.' \u00a9William Shakespeare. This saying resonates deeply with my journey through winding personal and professional paths. I believe that it is this diversity and versatility that form the cornerstone of success.\n\nBeing exposed to different professional fields, I bring a unique perspective to every challenge. My vision is to harness the power of varied skills and experiences to drive innovation and excellence. I am committed to fostering an environment where adaptability, continuous learning, and diverse expertise are celebrated as essential elements of growth and achievement.\n\nBy embracing and integrating these diverse pathways, I aim to contribute to a dynamic and inclusive world where versatility is recognized as a strength and a vital component of sustainable success."
+		'id': 'vision',
+		'title': "My Vision",
+		'description': "'A jack of all trades is a master of none, but oftentimes better than a master of one.' \u00a9William Shakespeare. This saying resonates deeply with my journey through winding personal and professional paths. I believe that it is this diversity and versatility that form the cornerstone of success.\n\nBeing exposed to different professional fields gives me incredable opportunity to bring a unique perspective to every challenge. My vision is to harness the power of varied skills and experiences to drive innovation and excellence. I am committed to fostering an environment where adaptability, continuous learning, and diverse expertise are celebrated as essential elements of growth and achievement.\n\nBy embracing and integrating these diverse pathways, I aim to contribute to a dynamic and inclusive world where versatility is recognized as a strength and a vital component of sustainable success.",
 	},
 	{
-		id: 'learns',
-		title: "My Learns",
-		description: "here is about whet I'm learning"
+		'id': 'learns',
+		'title': "My Learns",
+		'description': "here is about what I'm learning",
 	},
 	{
-		id: 'home',
-		title: "Home",
-		description: "home section"
+		'id': 'home',
+		'title': "Home",
+		'description': 'Hey! My name is Leila.\nEnjoy this art created by DALL-E.',
+		'image': art
 	}
 ]
